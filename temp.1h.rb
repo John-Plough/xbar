fcdbe3fa3c8a9dd3b6091753648a93e474a5ba8d
John-Plough <jplough@gmail.com>
2025-02-16 20:22:09 -0500

    got TC weather to work in Ruby file

--- temp.1h.rb
+++ temp.1h.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env /Users/plough/.rbenv/shims/ruby
+# <xbar.var>string(WEATHER_API_KEY=""): OpenWeather api key</xbar.var>
 require "http"
+
+city = "Traverse City"
 
 response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
 data = response.parse
@@ -8,7 +11,7 @@ temp = data["main"]["temp"].round
 high = data["main"]["temp_max"].round
 low = data["main"]["temp_min"].round
 
-puts temp
+puts "It's #{temp}°F in #{city}."
 puts "---"
-puts high
-puts low
+puts "Today's high: #{high}°F"
+puts "Today's low: #{low}°F"
